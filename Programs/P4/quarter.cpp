e05f33f9d5b0b41a2c225a018fbdf9b3cb4c3581
Eric Lowe <elowe@ucdavis.edu>
2012-07-16 16:30:27 -0700

    Adding source files

---
+++ Programs/P4/quarter.cpp
@@ -0,0 +1,17 @@
+// Eric Lowe and Rodolfo Hernandez
+// quarter.cpp
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cstdlib>
+#include <cstring>
+#include "course.h"
+#include "schedule.h"
+#include "quarter.h"
+
+using namespace std;
+
+
+
+
---
+++ Programs/P4/schedule.cpp
@@ -0,0 +1,16 @@
+// Eric Lowe and Rodolfo Hernandez
+// schedule.cpp
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cstdlib>
+#include <cstring>
+#include "catalog.h"
+#include "quarter.h"
+#include "schedule.h"
+
+using namespace std;
+
+
+
